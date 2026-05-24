e3242ae2d997af72c5c7d83231f4b90f107ea09c
Admin <ronghenidhi@gmail.com>
2026-05-24 10:23:22 +0530

    Rename to happyBirthdayyyy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/birthday-cake-magic/',
+  base: '/happyBirthdayyyy/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
